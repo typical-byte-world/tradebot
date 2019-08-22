2673080d828956708b0a1d9c41a2f2ce32f87ed2
Artem <artem.gagolkin22@gmail.com>
2019-08-22 12:38:31 +0300

    minor improvements

--- utils/image_convertor.py
+++ utils/image_convertor.py
@@ -3,12 +3,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib as mpl
 import os
-
+import time
 
 DATA_FOLDER = '../data'
-RESULTS = '../data/results'
+RESULTS = '../results'
 DATA = os.listdir(DATA_FOLDER)
-print(os.path.isfile(RESULTS))
 
 if not os.path.isdir(RESULTS):
     os.mkdir(RESULTS)
@@ -16,9 +15,11 @@ if not os.path.isdir(RESULTS):
 
 def save_image(data, dir, name):
     mpl.rcParams['savefig.pad_inches'] = 0
+
     ax = plt.axes([0, 0, 1, 1], frameon=False)
     ax.get_xaxis().set_visible(False)
     ax.get_yaxis().set_visible(False)
+
     plt.autoscale(tight=True)
 
     # Plot the data.
@@ -26,6 +27,7 @@ def save_image(data, dir, name):
     # Save the figure and display the figure.
     plt.savefig(f'{dir}/{name}')
     plt.show()
+    time.sleep(0.5)
 
     if os.path.isfile(f'{dir}/{name}'):
         print(f'File {name} succsesful saved!')
@@ -40,6 +42,7 @@ def main():
         name += '.png'
 
         save_image(data, RESULTS, name)
+
 
 if __name__ == '__main__':
     main()
