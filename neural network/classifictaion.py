e1dec53d2df9d4d49663d2a98108f85272c4941d
Artem <artem.gagolkin22@gmail.com>
2019-08-25 12:28:16 +0300

    added neural net in pytorch, improvemets in utils

--- neural network/classifictaion.py
+++ neural network/classifictaion.py
@@ -1,3 +1,199 @@
+import torch
+import torch.nn as nn
+from torchvision.transforms import transforms
+from torch.utils.data import DataLoader
+from torch.optim import Adam
+from torch.autograd import Variable
+import numpy as np
 import pandas as pd
+from PIL import Image
+import os
 
-print('Starting')
+class Unit(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(Unit,self).__init__()
+        self.conv = nn.Conv2d(in_channels=in_channels,kernel_size=3,out_channels=out_channels,stride=1,padding=1)
+        self.bn = nn.BatchNorm2d(num_features=out_channels)
+        self.relu = nn.ReLU()
+
+    def forward(self, input_):
+        output = self.conv(input_)
+        output = self.bn(output)
+        output = self.relu(output)
+
+        return output
+
+
+class SimpleNet(nn.Module):
+    def __init__(self, num_classes=3):
+        super(SimpleNet, self).__init__()
+
+        # Create 14 layers of the unit with max pooling in between
+        self.unit1 = Unit(in_channels=3, out_channels=32)
+        self.unit2 = Unit(in_channels=32, out_channels=32)
+        self.unit3 = Unit(in_channels=32, out_channels=32)
+
+        self.pool1 = nn.MaxPool2d(kernel_size=2)
+
+        self.unit4 = Unit(in_channels=32, out_channels=64)
+        self.unit5 = Unit(in_channels=64, out_channels=64)
+        self.unit6 = Unit(in_channels=64, out_channels=64)
+        self.unit7 = Unit(in_channels=64, out_channels=64)
+
+        self.pool2 = nn.MaxPool2d(kernel_size=2)
+
+        self.unit8 = Unit(in_channels=64, out_channels=128)
+        self.unit9 = Unit(in_channels=128, out_channels=128)
+        self.unit10 = Unit(in_channels=128, out_channels=128)
+        self.unit11 = Unit(in_channels=128, out_channels=128)
+
+        self.pool3 = nn.MaxPool2d(kernel_size=2)
+
+        self.unit12 = Unit(in_channels=128, out_channels=128)
+        self.unit13 = Unit(in_channels=128, out_channels=128)
+        self.unit14 = Unit(in_channels=128, out_channels=128)
+
+        self.avgpool = nn.AvgPool2d(kernel_size=4)
+
+        self.net = nn.Sequential(self.unit1, self.unit2, self.unit3, self.pool1, self.unit4, self.unit5, self.unit6
+                                 , self.unit7, self.pool2, self.unit8, self.unit9, self.unit10, self.unit11, self.pool3,
+                                 self.unit12, self.unit13, self.unit14, self.avgpool)
+
+        self.fc = nn.Linear(in_features=128 * 4 * 4, out_features=3)
+
+    def forward(self, input_):
+        output = self.net(input_)
+        output = output.view(output.size(0), -1)
+        output = self.fc(output)
+        return output
+
+
+class Data():
+    def __init__(self, path_to_csv=None):
+        self.data = pd.read_csv(path_to_csv)
+        self.transform = transforms.Compose([transforms.Resize((128, 128)),
+                                             transforms.ToTensor(),
+                                             transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+
+    def __len__(self):
+        return len(self.data)
+
+    def img_transform(self, img):
+        return self.transform(img)
+
+    def __getitem__(self, index):
+        img_path = self.data.iloc[index][0]
+        img_array = Image.open(img_path).convert('RGB')
+        X = self.img_transform(img_array)
+        y = self.data.iloc[index][1]
+        return X, y
+
+
+def adjust_learning_rate(epoch):
+    lr = 0.001
+    if epoch > 70:
+        lr = lr / 1000000
+    elif epoch > 50:
+        lr = lr / 100000
+    elif epoch > 40:
+        lr = lr / 10000
+    elif epoch > 30:
+        lr = lr / 1000
+    elif epoch > 20:
+        lr = lr / 100
+    elif epoch > 10:
+        lr = lr / 10
+
+    for param_group in optimizer.param_groups:
+        param_group["lr"] = lr
+
+
+def save_models(epoch):
+    if not os.path.isdir('models'):
+        os.mkdir('models')
+    torch.save(model.state_dict(), "mod2/model_{}.model".format(epoch))
+    print("Checkpoint saved")
+
+
+def test():
+    model.eval()
+    test_acc = 0.0
+    total = 0.0
+    correct = 0.0
+    for i, (images, labels) in enumerate(testloader):
+        # Predict classes using images from the test set
+        outputs = model(images)
+        _, prediction = torch.max(outputs.data, 1)
+        #         prediction = prediction.cpu().numpy()
+        total += labels.size(0)
+        test_acc += torch.sum(prediction == labels.data).item()
+
+    test_acc = 100 * test_acc / total
+
+    return test_acc
+
+
+def train_model(num_epochs):
+    best_acc = 0.0
+
+    for epoch in range(num_epochs):
+        model.train()
+        train_acc = 0.0
+        train_loss = 0.0
+        for i, (images, labels) in enumerate(trainloader):
+            # Move images and labels to gpu if availabl
+
+            # Clear all accumulated gradients
+            optimizer.zero_grad()
+            # Predict classes using images from the test set
+            outputs = model(images)
+            #             print(labels.shape)
+            #             print(outputs)
+            # Compute the loss based on the predictions and actual labels
+            loss = loss_fn(outputs, labels)
+            # Backpropagate the loss
+            loss.backward()
+
+            # Adjust parameters according to the computed gradients
+            optimizer.step()
+
+            train_loss += loss.item() * images.size(0)
+            _, prediction = torch.max(outputs.data, 1)
+
+            train_acc += torch.sum(prediction == labels.data)
+
+        # Call the learning rate adjustment function
+        adjust_learning_rate(epoch)
+
+        # Compute the average acc and loss over all 50000 training images
+        train_acc = train_acc / 500
+        train_loss = train_loss / 500
+
+        # Evaluate on the test set
+        test_acc = test()
+
+        # Save the model if the test acc is greater than our current best
+        if test_acc > best_acc:
+            save_models(epoch)
+            best_acc = test_acc
+
+        # Print the metrics
+        print("Epoch {}, Train Accuracy: {} , TrainLoss: {} , Test Accuracy: {}".format(epoch, train_acc, train_loss,
+                                                                                        test_acc))
+
+
+if __name__ == '__main__':
+    train = Data('train.csv')
+    test = Data('test.csv')
+
+    trainloader = torch.utils.data.DataLoader(train, batch_size=32,
+                                              shuffle=True, num_workers=4)
+    testloader = torch.utils.data.DataLoader(test, batch_size=32,
+                                             shuffle=True, num_workers=4)
+
+    model = SimpleNet(num_classes=3)
+
+    optimizer = Adam(model.parameters(), lr=0.001, weight_decay=0.0001)
+    loss_fn = nn.CrossEntropyLoss()
+
+    train(100)
---
+++ neural network/evaluation.py
@@ -0,0 +1,91 @@
+import torch
+import torch.nn as nn
+from torchvision.transforms import transforms
+from torch.autograd import Variable
+from PIL import Image
+
+
+class Unit(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(Unit, self).__init__()
+        self.conv = nn.Conv2d(in_channels=in_channels, kernel_size=3, out_channels=out_channels, stride=1, padding=1)
+        self.bn = nn.BatchNorm2d(num_features=out_channels)
+        self.relu = nn.ReLU()
+
+    def forward(self, input):
+        output = self.conv(input)
+        output = self.bn(output)
+        output = self.relu(output)
+
+        return output
+
+
+class SimpleNet(nn.Module):
+    def __init__(self, num_classes=3):
+        super(SimpleNet, self).__init__()
+
+        # Create 14 layers of the unit with max pooling in between
+        self.unit1 = Unit(in_channels=3, out_channels=32)
+        self.unit2 = Unit(in_channels=32, out_channels=32)
+        self.unit3 = Unit(in_channels=32, out_channels=32)
+
+        self.pool1 = nn.MaxPool2d(kernel_size=2)
+
+        self.unit4 = Unit(in_channels=32, out_channels=64)
+        self.unit5 = Unit(in_channels=64, out_channels=64)
+        self.unit6 = Unit(in_channels=64, out_channels=64)
+        self.unit7 = Unit(in_channels=64, out_channels=64)
+
+        self.pool2 = nn.MaxPool2d(kernel_size=2)
+
+        self.unit8 = Unit(in_channels=64, out_channels=128)
+        self.unit9 = Unit(in_channels=128, out_channels=128)
+        self.unit10 = Unit(in_channels=128, out_channels=128)
+        self.unit11 = Unit(in_channels=128, out_channels=128)
+
+        self.pool3 = nn.MaxPool2d(kernel_size=2)
+
+        self.unit12 = Unit(in_channels=128, out_channels=128)
+        self.unit13 = Unit(in_channels=128, out_channels=128)
+        self.unit14 = Unit(in_channels=128, out_channels=128)
+
+        self.avgpool = nn.AvgPool2d(kernel_size=4)
+
+        self.net = nn.Sequential(self.unit1, self.unit2, self.unit3, self.pool1, self.unit4, self.unit5, self.unit6
+                                 , self.unit7, self.pool2, self.unit8, self.unit9, self.unit10, self.unit11, self.pool3,
+                                 self.unit12, self.unit13, self.unit14, self.avgpool)
+
+        self.fc = nn.Linear(in_features=128 * 4 * 4, out_features=3)
+
+    def forward(self, input_):
+        output = self.net(input_)
+        output = output.view(output.size(0), -1)
+        output = self.fc(output)
+        return output
+
+
+def evaluate_model(img, model):
+    image = Image.open(img).convert('RGB')
+
+    transformation = transforms.Compose([transforms.Resize((128, 128)),
+                                         transforms.ToTensor(),
+                                         transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+
+                                         ])
+    image_tensor = transformation(image).float()
+    image_tensor = image_tensor.unsqueeze_(0)
+    inp = Variable(image_tensor)
+
+    output = model(inp)
+    result = output.argmax().item()
+
+    return result
+
+
+if __name__ == '__main__':
+    checkpoint = torch.load('model_4.model')
+    model = SimpleNet(3)
+    model.load_state_dict(checkpoint)
+    model.eval()
+
+    res = evaluate_model('path_to_img', model)
