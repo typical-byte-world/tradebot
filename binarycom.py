4d305a6ed3642164cfad8738c86bf30101d7bdaa
Dmytro Buhaiets <DmytroBuhaiets@gmail.com>
2019-08-25 13:39:11 +0300

    Regular commit.

--- binarycom.py
+++ binarycom.py
@@ -4,35 +4,35 @@ import math
 import websockets
 
 
-def connect(app_id: int):
-    return websockets.connect(f'wss://ws.binaryws.com/websockets/v3?app_id={app_id}')
+async def connect(app_id: int):
+    return await websockets.connect(f'wss://ws.binaryws.com/websockets/v3?app_id={app_id}')
 
 
-async def _do(websocket: websockets.client, request: dict) -> dict:
+async def _do(websocket, request: dict) -> dict:
     await websocket.send(json.dumps(request))
     return json.loads(await websocket.recv())
 
 
-async def authorize(websocket: websockets.client, api_token: str) -> dict:
+async def authorize(websocket, api_token: str) -> dict:
     request = {
-        "authorize": api_token
+        'authorize': api_token
     }
     return await _do(websocket, request)
 
 
-async def tick_history(websocket: websockets.client, symbol: str, from_: int, to: int) -> dict:
+async def tick_history(websocket, symbol: str, from_: int, to: int) -> dict:
     request = {
-        "tick_history": symbol,
-        "start": from_,
-        "end": to
+        'tick_history': symbol,
+        'start': from_,
+        'end': to
     }
     return await _do(websocket, request)
 
 
-async def buy_contract(websocket: websockets.client, parameters: dict) -> dict:
+async def buy_contract(websocket, parameters: dict) -> dict:
     request = {
-        "buy": 1,
-        "price": math.inf,
-        "parameters": parameters
+        'buy': 1,
+        'price': math.inf,
+        'parameters': parameters
     }
     return await _do(websocket, request)
