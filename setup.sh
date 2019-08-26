da5ce2d2b4f983f80745056b11cbfb2db40f22ca
Dmytro Buhaiets <DmytroBuhaiets@gmail.com>
2019-08-26 15:04:54 +0300

    Regular commit.

--- setup.sh
+++ setup.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew install python3
+
 python3 -m venv tradebotvenv
 source tradebotvenv/bin/activate
 pip3 install -r requirements.txt
